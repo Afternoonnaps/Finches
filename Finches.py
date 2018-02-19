dec30a24f3bdbf20c98f9875f428435a4a7a52a5
Jinyan Jiang <jinyanj@berkeley.edu>
2018-02-18 16:41:48 -0800

    BD BL

--- Finches.py
+++ Finches.py
@@ -123,3 +123,103 @@ _ = plt.legend(('1975', '2012'), loc='upper left')
 plt.show()
 
 
+
+#LIN REG OF BEAK LENGTH AND DEPTH
+
+def draw_bs_pairs_linreg(x, y, size=1):
+    """Perform pairs bootstrap for linear regression."""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_slope_reps, bs_intercept_reps
+    bs_slope_reps = np.empty(size)
+    bs_intercept_reps = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds, size=len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_slope_reps[i], bs_intercept_reps[i] = np.polyfit(bs_x,bs_y,1)
+
+    return bs_slope_reps, bs_intercept_reps
+
+# Compute the linear regressions
+slope_1975, intercept_1975 = np.polyfit(bl_1975, bd_1975, 1)
+slope_2012, intercept_2012 = np.polyfit(bl_2012, bd_2012, 1)
+
+# Perform pairs bootstrap for the linear regressions
+bs_slope_reps_1975, bs_intercept_reps_1975 = draw_bs_pairs_linreg(bl_1975, bd_1975, 1000)
+bs_slope_reps_2012, bs_intercept_reps_2012 = draw_bs_pairs_linreg(bl_2012, bd_2012, 1000)
+
+# Compute confidence intervals of slopes
+slope_conf_int_1975 = np.percentile(bs_slope_reps_1975, [2.5, 97.5])
+slope_conf_int_2012 = np.percentile(bs_slope_reps_2012, [2.5, 97.5])
+intercept_conf_int_1975 = np.percentile(bs_intercept_reps_1975, [2.5, 97.5])
+intercept_conf_int_2012 = np.percentile(bs_intercept_reps_2012, [2.5, 97.5])
+
+# Print the results
+print('1975: slope =', slope_1975,
+      'conf int =', slope_conf_int_1975)
+print('1975: intercept =', intercept_1975,
+      'conf int =', intercept_conf_int_1975)
+print('2012: slope =', slope_2012,
+      'conf int =', slope_conf_int_2012)
+print('2012: intercept =', intercept_2012,
+      'conf int =', intercept_conf_int_2012)
+
+
+# Make scatter plot of 1975 data
+_ = plt.plot(bl_1975, bd_1975, marker='.',
+             linestyle='none', color='blue', alpha=0.5)
+
+# Make scatter plot of 2012 data
+_ = plt.plot(bl_2012, bd_2012, marker='.',
+             linestyle='none', color='red', alpha=0.5)
+
+# Label axes and make legend
+_ = plt.xlabel('beak length (mm)')
+_ = plt.ylabel('beak depth (mm)')
+_ = plt.legend(('1975', '2012'), loc='upper left')
+
+# Generate x-values for bootstrap lines: x
+x = np.array([10, 17])
+
+# Plot the bootstrap lines
+for i in range(100):
+    plt.plot(x, bs_slope_reps_1975[i] * x + bs_intercept_reps_1975[i],
+             linewidth=0.5, alpha=0.2, color='blue')
+    plt.plot(x, bs_slope_reps_2012[i] * x + bs_intercept_reps_2012[i],
+             linewidth=0.5, alpha=0.2, color='red')
+
+# Draw the plot again
+plt.show()
+
+
+#BEAK LENGTH TO DEPTH RATIO
+
+# Compute length-to-depth ratios
+ratio_1975 = bl_1975 / bd_1975
+ratio_2012 = bl_2012 / bd_2012
+
+# Compute means
+mean_ratio_1975 = np.mean(ratio_1975)
+mean_ratio_2012 = np.mean(ratio_2012)
+
+# Generate bootstrap replicates of the means
+bs_replicates_1975 = draw_bs_reps(ratio_1975, np.mean, size=10000)
+bs_replicates_2012 = draw_bs_reps(ratio_2012, np.mean, size=10000)
+
+# Compute the 99% confidence intervals
+conf_int_1975 = np.percentile(bs_replicates_1975,[.5,99.5])
+conf_int_2012 = np.percentile(bs_replicates_2012,[.5,99.5])
+
+# Print the results
+print('1975: mean ratio =', mean_ratio_1975,
+      'conf int =', conf_int_1975)
+print('2012: mean ratio =', mean_ratio_2012,
+      'conf int =', conf_int_2012)
+
+
+
+
